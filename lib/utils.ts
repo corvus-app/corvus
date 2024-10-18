4758e2c9bc04014146a497fe2b4ae1fd18e3ac7b
szhen0340 <cowtastical@gmail.com>
2024-10-18 02:15:07 -0500

    feat: added excluded extensions function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,14 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+const excludedExtensions = ["png", "jpg", "jpeg", "ico", ".woff"];
+export function isExcludedFile(fileName: string) {
+  return (
+    fileName === "package-lock.json" ||
+    excludedExtensions.some((ext) => fileName.endsWith(ext))
+  );
 }
